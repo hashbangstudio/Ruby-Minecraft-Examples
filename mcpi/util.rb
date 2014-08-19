9b77f86cdcb217d22da71f9cd802346e79d932e0
HashBangStudio <hashbangstudio@users.noreply.github.com>
2014-08-19 19:55:49 +0000

    Fix for return of function
    Make it so function would return value if passed non-iterable
    Add comment to explain flatten in ruby is recursive

--- mcpi/util.rb
+++ mcpi/util.rb
@@ -1,7 +1,10 @@
 def flatten_parameters_to_string(l)
+    #flatten is recursive in ruby
     if l.respond_to? :each
         val =  l.flatten.join(",")
+        return val
+    else
+       return l;
     end
-    #puts "val", val
-    return val
 end
+
